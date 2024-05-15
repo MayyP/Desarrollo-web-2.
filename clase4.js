a42adb68d2ba1d1c952b3e18ed7c8fc797ab1d4d
MayyP <pincirolimayra@gmail.com>
2024-05-15 16:31:02 -0300

    clase4

--- clase4.js
+++ clase4.js
@@ -1,8 +1,9 @@
 let NombreProducto = "Pantalones"
 let PrecioUnitario = 500
+let CantidadDeseada = parseInt (prompt("Ingrese la cantidad deseada de" + (NombreProducto) + "que desea comprar:"))
 function SumarProductos (PrecioUnitario, CantidadDeseada){
     let TotalGastado = PrecioUnitario * CantidadDeseada
     console. log (TotalGastado)
 }
-let CantidadDeseada = parseInt (prompt("Ingrese la cantidad deseada de" + (NombreProducto) + "que desea comprar:"))
+
 SumarProductos (PrecioUnitario, CantidadDeseada)
