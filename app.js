eb24513b0dee3fcbc553282ea8b88aae6a5241d0
MayyP <pincirolimayra@gmail.com>
2024-05-31 18:44:00 -0300

    clase7

---
+++ app.js
@@ -0,0 +1,5 @@
+let precioP = document.getElementById("precioProducto")
+precioP.innerText = "precio: $450";
+let titulo = document.querySelector("#nombreProducto")
+titulo.style.color = "red"
+titulo.classList.add/("color","fondo")
